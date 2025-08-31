a8e3ecab2e76363476392a27f1655f24f09938a4
TanushTiwari <dr.amritkumar.maa.janki@gmail.com>
2025-08-31 10:23:27 +0530

    This part OOPS is taking time to get deeep into it

---
+++ GitHub-Release/ALPHA5/OOPs/getterSEtter.java
@@ -0,0 +1,69 @@
+package OOPs;
+
+public class getterSEtter {
+    public static void main(String[] args) {
+
+        Pen p1= new Pen(); // created a pen object called p1
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.color="Yellow"; // Directly changing the property of the object
+        System.out.println(p1.getColor());
+
+        BankAccount b1=new BankAccount();
+        b1.username="Vikash";
+        b1.setPassword("abcd1234");
+    }
+    
+}
+class Pen{
+    //Properties
+    String color;
+    int tip;
+    //Functions
+    void setColor(String newColor){
+        color=newColor;
+    }
+    void setTip(int newTip){
+        tip=newTip;
+    }
+}
+class BankAccount{
+    public String username;
+    private String password;
+    int accountNo;
+    protected float balance;
+
+    void deposit(float amount){
+        balance=balance+amount;
+    }
+    boolean withdraw(float amount){
+        if(balance>=amount){
+            balance=balance-amount;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    float checkBalance(){
+        return balance;
+    }
+    void setPassword(String newPassword){
+        password=newPassword;
+    }
+}
+class Student{
+    String name;
+    int age;
+    float percentage;
+
+    void caclPercentage(int phy, int chem, int math){
+        percentage=(phy+chem+math)/3;
+    }
+}
+
+/*
+This part is taking time                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              '
+*/
